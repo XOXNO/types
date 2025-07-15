8eda68b25717d0bec609c3c6b1e66bd97a45bdc1
Boris Pöhland <me@boris.app>
2025-07-15 12:25:33 +0200

    lending indexes dto

---
+++ src/requests/lending/lending-indexes.dto.ts
@@ -0,0 +1,10 @@
+export class LendingIndexesDto {
+  supplyIndex!: string;
+  supplyIndexShort!: number;
+  borrowIndex!: string;
+  borrowIndexShort!: number;
+  egldPrice!: string;
+  egldPriceShort!: number;
+  usdPrice!: string;
+  usdPriceShort!: number;
+}
