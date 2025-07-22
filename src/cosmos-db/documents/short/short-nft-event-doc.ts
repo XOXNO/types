dae32cd84f49e9e0bdf9ab50a990822c68ea2fa2
Boris Pöhland <me@boris.app>
2025-07-22 12:01:41 +0200

    add new type

---
+++ src/cosmos-db/documents/short/short-nft-event-doc.ts
@@ -0,0 +1,9 @@
+import { IntersectionType, PickType } from '@nestjs/swagger';
+import { NftDocHydrated } from '../token/nft-details.doc';
+import { NftSaleInfoHydrated } from '../token/nft-sale-info';
+
+export class ShortNftEventDoc extends PickType(NftDocHydrated, [
+  'identifier',
+] as const) {
+  saleInfo?: Pick<NftSaleInfoHydrated, 'minBidShort' | 'paymentToken'>;
+}
