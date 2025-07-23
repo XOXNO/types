b6fc839d4ecb88157c266aac9328ac35040c5595
Boris Pöhland <me@boris.app>
2025-07-23 14:37:46 +0200

    make it array

--- src/entities/delegation-data/delegation-data-output.ts
+++ src/entities/delegation-data/delegation-data-output.ts
@@ -10,7 +10,7 @@ export class DelegationDataOutput {
   contract!: string;
   activeStake!: string; // Assuming activeStake is a string, adjust the type if it's different
   activeStakeShort!: number;
-  unDelegateInfo!: UndelegateInfo; // Replace 'any' with the appropriate type if known
+  unDelegateInfo!: UndelegateInfo[]; // Replace 'any' with the appropriate type if known
   claimableRewards!: number; // Assuming claimableRewards is a number, adjust the type if it's different
   claimableRewardsShort!: number; // Assuming claimableRewardsShort is a string, adjust the type if it's different
   totalUnbondEligible!: string;
