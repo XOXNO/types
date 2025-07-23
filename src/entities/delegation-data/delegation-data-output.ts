57253a97a0a2c9fa806150dede258b70642e6711
Boris Pöhland <me@boris.app>
2025-07-23 14:32:42 +0200

    retype DelegationDataOutput

--- src/entities/delegation-data/delegation-data-output.ts
+++ src/entities/delegation-data/delegation-data-output.ts
@@ -1,17 +1,17 @@
-export interface UndelegateInfo {
-  id: string;
-  value: string;
-  timestamp: number;
-  valueShort: number;
+export class UndelegateInfo {
+  id!: string;
+  value!: string;
+  timestamp!: number;
+  valueShort!: number;
 }
 
-export interface DelegationDataOutput {
-  address: string;
-  contract: string;
-  activeStake: string; // Assuming activeStake is a string, adjust the type if it's different
-  activeStakeShort: number;
-  unDelegateInfo: UndelegateInfo; // Replace 'any' with the appropriate type if known
-  claimableRewards: number; // Assuming claimableRewards is a number, adjust the type if it's different
-  claimableRewardsShort: number; // Assuming claimableRewardsShort is a string, adjust the type if it's different
-  totalUnbondEligible: string;
+export class DelegationDataOutput {
+  address!: string;
+  contract!: string;
+  activeStake!: string; // Assuming activeStake is a string, adjust the type if it's different
+  activeStakeShort!: number;
+  unDelegateInfo!: UndelegateInfo; // Replace 'any' with the appropriate type if known
+  claimableRewards!: number; // Assuming claimableRewards is a number, adjust the type if it's different
+  claimableRewardsShort!: number; // Assuming claimableRewardsShort is a string, adjust the type if it's different
+  totalUnbondEligible!: string;
 }
