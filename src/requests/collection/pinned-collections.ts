5fbabc68ada6a6c2f5f2971d5538cb1c8b7baf44
Boris Pöhland <me@boris.app>
2025-07-23 16:03:32 +0200

    change PinnedCollectionDto

--- src/requests/collection/pinned-collections.ts
+++ src/requests/collection/pinned-collections.ts
@@ -1,38 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
+import { CollectionProfileDoc } from '../../cosmos-db/documents/collection/profile';
 
-export class PinnedCollectionDto {
-  @ApiProperty({ example: 'EAPES-8f3c1f' })
-  collection!: string;
-
-  @ApiProperty({ example: 'EAPES' })
-  name!: string;
-
-  @ApiProperty({ example: true })
-  isVerified!: boolean;
-
-  @ApiProperty({
-    example:
-      'https://media.xoxno.com/collectionprofile/EAPES-8f3c1f/profilePicture.webp?ts=1691359925',
-  })
-  profile!: string;
-
-  @ApiProperty({
-    example:
-      'https://media.xoxno.com/collectionprofile/EAPES-8f3c1f/profilebanner.webp',
-  })
-  banner!: string;
-
-  @ApiProperty({
-    example:
-      'EAPES is a collection of 10,000 unique 3D APEs living on the MultiversX blockchain',
-  })
-  description!: string;
-
-  @ApiProperty({
-    example: 'erd1qqqqqqqqqqqqqpgq7gma4udjeh3mwuq4lmk7wccgh0tenxu6yv5qc2csfy',
-  })
-  creator!: string;
-
-  @ApiProperty({ example: false, required: false })
-  isMintable?: boolean;
-}
+export class PinnedCollectionDto extends PickType(CollectionProfileDoc, [
+  'collection',
+  'name',
+  'isVerified',
+  'profile',
+  'banner',
+  'description',
+  'creator',
+  'isMintable',
+  'chain',
+] as const) {}
