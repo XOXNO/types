dacce4bf408df72cf9f31527ae2918ba01241e2e
Boris Pöhland <me@boris.app>
2025-11-28 11:24:16 +0100

    add types for message sign

---
+++ src/entities/passkey/response.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SignMessageFailureDto {
+  @ApiProperty({ example: false })
+  success!: false;
+}
+
+export class SignMessageSuccessDto {
+  @ApiProperty({ example: true })
+  success!: true;
+
+  @ApiProperty()
+  authenticatorData!: string;
+
+  @ApiProperty()
+  clientDataJSON!: string;
+
+  @ApiProperty()
+  signature!: string;
+}
+
+export type SignMessageDto = SignMessageSuccessDto | SignMessageFailureDto;
